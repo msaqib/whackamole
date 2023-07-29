35bfeaea61af383ae74c5516567e1191a0ae0e44
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-07-29 13:11:01 +0500

    Added remove and add functions. Refactored the code.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-let randCell = Math.floor(Math.random()*9) + 1 
+// Create an img element for the mole image
 const mole = document.createElement('img')
 mole.setAttribute('src', 'animal.png')
+// Pick a cell at random
+let randCell = Math.floor(Math.random()*9) + 1 
+
+// Display the mole in the randomly picked cell
 const cell = document.getElementById(randCell)
 cell.appendChild(mole)
+
+let timeout = 800 + Math.floor(Math.random()*2000)
+setTimeout(removeMole, timeout)
+
+function removeMole() {
+    cell.removeChild(cell.firstChild)
+}
